01499286731481d21ccc2d53bc64e63a36834e2c
isaacmtz <isaac.amr15@gmail.com>
2024-03-08 15:02:09 -0600

    Initial commit.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+# Iniciar ambiente virtual
+subprocess.call(['python', '-m', 'venv', 'venv'])
+
+# Path to a Python interpreter that runs any Python script
+# under the virtualenv /path/to/virtualenv/
+python_venv = os.getcwd()+"\\venv\\Scripts\\python.exe"
+
+subprocess.call([python_venv,'-m', 'pip', '--upgrade', 'pip'])
+subprocess.call([python_venv,'-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+
+# Configurar el ambiente para recibir notebooks.
+
+if'{{ cookiecutter.project_packages }}' == 'Notebook':
+  subprocess.call([python_venv,'-m', 'ipykernel', 'install', '--user', '--name', 'venv'])
+
+# Iniciar git
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
